8f5c2d67e99ec16a0a6951f68e31ab9e525fa200
Andrés Banda <andrezh_gln7@live.com>
2024-06-06 01:04:56 -0500

    feat: create overview page to extract from app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import "./App.css";
-import AccountSummary from "./components/AccountSummary";
-import SidebarMenu from "./components/SidebarMenu";
-import { TransactionOverview } from "./components/Transaction";
-import TransactionList from "./components/TransactionList";
-import transactionsData from "./data/transactions.json";
-
-const transactions: TransactionOverview[] =
-  transactionsData as TransactionOverview[];
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <div className="grid grid-cols-12">
-        <SidebarMenu />
-        <main className="col-span-10 grid grid-cols-6">
-          <AccountSummary />
-          <TransactionList transactions={transactions} />
-        </main>
-      </div>
+      <HomePage />
     </>
   );
 }
---
+++ src/pages/OverviewPage.tsx
@@ -0,0 +1,24 @@
+import AccountSummary from "../components/AccountSummary";
+import SidebarMenu from "../components/SidebarMenu";
+import { ITransactionOverview } from "../components/TransactionOverview";
+import TransactionOverviewList from "../components/TransactionOverviewList";
+import transactionsData from "../data/transactions.json";
+
+const transactions: ITransactionOverview[] =
+  transactionsData as ITransactionOverview[];
+
+function OverviewPage() {
+  return (
+    <>
+      <div className="grid grid-cols-12 bg-neutral-50 dark:bg-neutral-800">
+        <SidebarMenu />
+        <main className="col-span-10 md:grid md:grid-cols-6 pt-2">
+          <AccountSummary />
+          <TransactionOverviewList transactions={transactions} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default OverviewPage;

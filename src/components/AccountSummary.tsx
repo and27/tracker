acc9a5e20871a7d9da161dce0f6631d521c5662f
Andrés Banda <andrezh_gln7@live.com>
2024-06-06 01:02:16 -0500

    feat: create SummaryCard component to consolidate Summary page info

--- src/components/AccountSummary.tsx
+++ src/components/AccountSummary.tsx
@@ -1,26 +1,28 @@
 import PieChart from "./PieChart";
 
+type SummaryCardProps = {
+  title: string;
+  value: string;
+};
+
+const SummaryCard = ({ title, value }: SummaryCardProps) => {
+  return (
+    <div className="shadow p-5 bg-white dark:bg-neutral-800 rounded">
+      <p className="text-sm text-gray-500">{title}</p>
+      <p className="text-lg font-semibold">{value}</p>
+    </div>
+  );
+};
+
 const AccountSummary = () => {
   return (
-    <section className="col-span-4 p-6">
-      <h2 className="text-3xl mb-6">Account Summary</h2>
+    <section className="col-span-4 p-6 bg-neutral-50 dark:bg-neutral-900">
+      <h2 className="text-2xl lg:text-3xl mb-6">Account Summary</h2>
       <div className="grid grid-cols-2 gap-4">
-        <div className="shadow p-5">
-          <p className="text-sm text-gray-500">Current Balance</p>
-          <p className="text-lg font-semibold">$5,000.00</p>
-        </div>
-        <div className="shadow p-5">
-          <p className="text-sm text-gray-500">Expenses</p>
-          <p className="text-lg font-semibold">$5,000.00</p>
-        </div>
-        <div className="shadow p-5">
-          <p className="text-sm text-gray-500">Income</p>
-          <p className="text-lg font-semibold">$5,000.00</p>
-        </div>
-        <div className="shadow p-5">
-          <p className="text-sm text-gray-500">Total Transactions</p>
-          <p className="text-lg font-semibold">10</p>
-        </div>
+        <SummaryCard title="Income" value="$2,000" />
+        <SummaryCard title="Expense" value="$1,000" />
+        <SummaryCard title="Balance" value="$1,000" />
+        <SummaryCard title="Total Transactions" value="10" />
       </div>
 
       <div style={{ height: "500px" }}>

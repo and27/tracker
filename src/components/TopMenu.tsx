426cff8a7423cd97ef010be56365ef5d8aa451e1
Andrés Banda <andrezh_gln7@live.com>
2024-06-09 01:16:32 -0500

    feat: use logo in login and homepage

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,8 +3,8 @@ import LinkButton from "./LinkButton";
 
 const TopMenu = () => {
   return (
-    <nav className="grid grid-cols-3 items-center gap-10 px-12 py-4 shadow-sm bg-white">
-      <p className="font-semibold">Defispot</p>
+    <nav className="grid grid-cols-3 items-center gap-10 px-12 py-4 shadow-sm bg-white z-10 relative">
+      <img src="/logo.svg" alt="logo" width="50" height="50" />
       <ul className="flex justify-around text-slate-600 justify-self-stretch">
         <li>
           <Link to="/">Home</Link>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,16 +2,25 @@ import LoginForm from "../components/Forms/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div
-      className="bg-white dark:bg-neutral-800 shadow 
+    <>
+      <img
+        src="/logo.svg"
+        alt="logo"
+        width="100"
+        height="100"
+        className="mx-auto"
+      />
+      <div
+        className="bg-white dark:bg-neutral-800 shadow 
     p-10 rounded w-1/2 sm:w-1/3 mx-auto "
-    >
-      <h1 className="text-center text-3xl font-bold">Sign in</h1>
-      <p className="text-center text-neutral-500 pt-1 pb-4 text-lg">
-        to continue to Tracker
-      </p>
-      <LoginForm />
-    </div>
+      >
+        <h1 className="text-center text-3xl font-bold">Sign in</h1>
+        <p className="text-center text-neutral-500 pt-1 pb-4 text-lg">
+          to continue to Tracker
+        </p>
+        <LoginForm />
+      </div>
+    </>
   );
 };
 

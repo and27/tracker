1c45fd59ae7efe7d4f6204362fe19e671ad440d2
Andrés Banda <andrezh_gln7@live.com>
2024-06-18 09:46:22 -0500

    feat: add pie wrapper to provideinput data

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,84 +1,88 @@
 import { ResponsivePie } from "@nivo/pie";
 
-const data = [
-  { id: "java", label: "Java", value: 29 },
-  { id: "python", label: "Python", value: 25 },
-  { id: "javascript", label: "JavaScript", value: 20 },
-  { id: "c++", label: "C++", value: 15 },
-  { id: "ruby", label: "Ruby", value: 11 },
-];
+export type PieChartDataType = {
+  id: string;
+  label: string;
+  value: number;
+};
 
-const PieChart = () => (
-  <ResponsivePie
-    data={data}
-    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-    innerRadius={0.5}
-    padAngle={0.7}
-    cornerRadius={3}
-    colors={{ scheme: "nivo" }}
-    borderWidth={1}
-    borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-    animate={true}
-    defs={[
-      {
-        id: "dots",
-        type: "patternDots",
-        background: "inherit",
-        color: "rgba(255, 255, 255, 0.3)",
-        size: 4,
-        padding: 1,
-        stagger: true,
-      },
-      {
-        id: "lines",
-        type: "patternLines",
-        background: "inherit",
-        color: "rgba(255, 255, 255, 0.3)",
-        rotation: -45,
-        lineWidth: 6,
-        spacing: 10,
-      },
-    ]}
-    fill={[
-      {
-        match: {
-          id: "ruby",
+type PieChartProps = {
+  data: PieChartDataType[];
+};
+
+const PieChart = ({ data }: PieChartProps) => {
+  return (
+    <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={0.7}
+      cornerRadius={3}
+      colors={{ scheme: "nivo" }}
+      borderWidth={1}
+      borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
+      animate={true}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          size: 4,
+          padding: 1,
+          stagger: true,
         },
-        id: "dots",
-      },
-      {
-        match: {
-          id: "c++",
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "rgba(255, 255, 255, 0.3)",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
         },
-        id: "lines",
-      },
-    ]}
-    legends={[
-      {
-        anchor: "bottom",
-        direction: "row",
-        justify: false,
-        translateX: 0,
-        translateY: 56,
-        itemsSpacing: 0,
-        itemWidth: 100,
-        itemHeight: 18,
-        itemTextColor: "#999",
-        itemDirection: "left-to-right",
-        itemOpacity: 1,
-        symbolSize: 18,
-        symbolShape: "circle",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemTextColor: "#000",
-            },
+      ]}
+      fill={[
+        {
+          match: {
+            id: "ruby",
           },
-        ],
-      },
-    ]}
-  />
-);
+          id: "dots",
+        },
+        {
+          match: {
+            id: "c++",
+          },
+          id: "lines",
+        },
+      ]}
+      legends={[
+        {
+          anchor: "bottom",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: "#999",
+          itemDirection: "left-to-right",
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: "circle",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemTextColor: "#000",
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
 
 export default PieChart;
---
+++ src/components/PieChartDataProvider.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import PieChart, { PieChartDataType } from "./PieChart";
+import { getTransactions } from "../utils/supabaseDB";
+import { Transaction } from "../pages/TransactionsPage";
+
+const PieChartDataProvider = () => {
+  const [data, setData] = useState<PieChartDataType[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const { data: transactions, error } = await getTransactions();
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (transactions) {
+        const data = transactions.reduce(
+          (acc: PieChartDataType[], transaction: Transaction) => {
+            const existingCategory = acc.find(
+              (category) => category.id === transaction.category
+            );
+            if (existingCategory) {
+              existingCategory.value += transaction.amount;
+            } else {
+              acc.push({
+                id: transaction.category,
+                label: transaction.category,
+                value: transaction.amount,
+              });
+            }
+            return acc;
+          },
+          []
+        );
+        setData(data);
+      }
+    };
+    fetchTransactions();
+  }, []);
+
+  return <PieChart data={data} />;
+};
+
+export default PieChartDataProvider;

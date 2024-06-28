be861b80e2f655a691ce5a498619b427f3441b95
Andrés Banda <andrezh_gln7@live.com>
2024-06-28 00:23:32 -0500

    feat: add pagination ui

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -56,6 +56,59 @@ const Table = ({ columns, data }: TableProps) => {
           ))}
         </tbody>
       </table>
+      <div className="p-3">
+        <div className="flex items-center gap-2">
+          <button
+            className="border rounded bg-neutral-900"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<<"}
+          </button>
+          <button
+            className="border rounded bg-neutral-900"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            {"<"}
+          </button>
+          <button
+            className="border rounded bg-neutral-900"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">"}
+          </button>
+          <button
+            className="border rounded bg-neutral-900"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            {">>"}
+          </button>
+          <span className="flex items-center gap-1">
+            <div>Page</div>
+            <strong>
+              {table.getState().pagination.pageIndex + 1} of{" "}
+              {table.getPageCount().toLocaleString()}
+            </strong>
+          </span>
+
+          <select
+            value={table.getState().pagination.pageSize}
+            className="border rounded bg-neutral-900 text-neutral-100 dark:bg-neutral-800 dark:text-neutral-100 p-1"
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 };

e23a3858da8c8d9661433b7a1cf1d96355b641f8
Andrés Banda <andrezh_gln7@live.com>
2024-06-28 00:24:38 -0500

    style: add hover underline on navbar items

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,16 +3,16 @@ import LinkButton from "./LinkButton";
 
 const TopMenu = () => {
   return (
-    <nav className="grid grid-cols-3 items-center gap-10 px-12 py-4 shadow-sm bg-white dark:bg-neutral-900 dark:text-white z-10 relative">
+    <nav className="grid grid-cols-3 items-center gap-10 px-12 shadow-sm bg-white dark:bg-neutral-900 dark:text-white z-10 relative">
       <img src="/logoCard.svg" alt="logo" width="24" height="24" />
-      <ul className="flex justify-around justify-self-stretch">
-        <li>
+      <ul className="flex justify-around justify-self-stretch text-neutral-700 dark:text-neutral-300 ">
+        <li className="hover:underline active:underline hover:animate-pulse p-5">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="hover:underline active:underline hover:animate-pulse p-5">
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li className="hover:underline active:underline hover:animate-pulse p-5">
           <Link to="/contact">Contact</Link>
         </li>
       </ul>

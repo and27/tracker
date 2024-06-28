ff91ddc6acb541d3c15c8e62177bf5d961bfa747
Andrés Banda <andrezh_gln7@live.com>
2024-06-28 16:01:54 -0500

    style: modify login light colors and mobile spacing

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,25 +2,26 @@ import LoginForm from "../components/Forms/LoginForm";
 
 const LoginPage = () => {
   return (
-    <>
-      <img
-        src="/logoCard.svg"
-        alt="logo"
-        width="50"
-        height="50"
-        className="mx-auto mb-5"
-      />
-      <div
-        className="bg-white dark:bg-neutral-800 shadow 
-    p-10 rounded w-1/2 sm:w-1/3 mx-auto "
-      >
-        <h1 className="text-center text-3xl font-bold font-outfit">Sign in</h1>
-        <p className="text-center text-neutral-500  dark:text-neutral-400 pt-1 pb-4 text-lg">
-          to continue to Tracker
-        </p>
-        <LoginForm />
+    <section className="h-screen pattern bg-neutral-50 dark:bg-neutral-900 grid items-center">
+      <div>
+        <img
+          src="/logoCard.svg"
+          alt="logo"
+          width="50"
+          height="50"
+          className="mx-auto mb-5"
+        />
+        <div className="bg-white dark:bg-neutral-800 shadow p-10 rounded sm:w-1/2 lg:w-1/3 sm:mx-auto mx-5">
+          <h1 className="text-center text-3xl font-bold font-outfit">
+            Sign in
+          </h1>
+          <p className="text-center text-neutral-500 dark:text-neutral-400 pt-1 pb-4 text-lg">
+            to continue to Tracker
+          </p>
+          <LoginForm />
+        </div>
       </div>
-    </>
+    </section>
   );
 };
 

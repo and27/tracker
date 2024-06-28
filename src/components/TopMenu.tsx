73658191de7229cb5d51d67badcf174ea9cfbc6d
Andrés Banda <andrezh_gln7@live.com>
2024-06-28 16:08:56 -0500

    remove menu items for demo

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,21 +1,13 @@
-import { Link } from "react-router-dom";
 import LinkButton from "./LinkButton";
 
 const TopMenu = () => {
   return (
-    <nav className="grid grid-cols-3 items-center gap-10 px-12 shadow-sm bg-white dark:bg-neutral-900 dark:text-white z-10 relative">
+    <nav
+      className="flex items-center px-12 shadow-sm bg-white dark:bg-neutral-900 
+    dark:text-white z-10 relative justify-between py-3"
+    >
       <img src="/logoCard.svg" alt="logo" width="24" height="24" />
-      <ul className="flex justify-around justify-self-stretch text-neutral-700 dark:text-neutral-300 ">
-        <li className="hover:underline active:underline hover:animate-pulse p-5">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="hover:underline active:underline hover:animate-pulse p-5">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="hover:underline active:underline hover:animate-pulse p-5">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+
       <LinkButton to="/login" className="justify-self-end">
         Login
       </LinkButton>

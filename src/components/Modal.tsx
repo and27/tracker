be1a6a0464a8a9aa25014a5598798fb92147b079
Andrés Banda <andrezh_gln7@live.com>
2024-07-21 11:51:37 -0500

    feat: add modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,34 @@
+import { FaTimes } from "react-icons/fa";
+
+interface ModalProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+}
+
+const Modal = ({ children, isOpen, onClose, title }: ModalProps) => {
+  if (isOpen)
+    return (
+      <div
+        className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center`}
+      >
+        <div
+          className={`flex flex-col gap-8 bg-neutral-100 dark:bg-neutral-800 w-96 min-h-32 p-4 rounded-lg shadow-lg`}
+        >
+          <div className="flex items-start">
+            <h2>{title}</h2>
+            <button
+              className="focus:outline-none bg-transparent p-2"
+              onClick={onClose}
+            >
+              <FaTimes />
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+    );
+};
+
+export default Modal;

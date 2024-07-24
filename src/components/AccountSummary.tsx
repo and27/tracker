1ec375f04ce6ae4027942c7bdf17ac7fe214fc0b
Andrés Banda <andrezh_gln7@live.com>
2024-07-24 00:09:18 -0500

    feat: calculate income, expenses, and account summary info

--- src/components/AccountSummary.tsx
+++ src/components/AccountSummary.tsx
@@ -17,25 +17,45 @@ const SummaryCard = ({ title, value }: SummaryCardProps) => {
   );
 };
 
-const AccountSummary = async () => {
-  const [totalTransactions, setTotalTransactions] = useState<number>(0);
-  const user = localStorage.getItem("userId") as string;
+const AccountSummary = () => {
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalTransactions, setTotalTransactions] = useState(0);
 
   useEffect(() => {
-    const fetchTotalTransactions = async () => {
-      const { data } = await getTransactions(user);
-      if (data) setTotalTransactions(data.length);
+    const setupAccountInfo = async () => {
+      const user = localStorage.getItem("userId") as string;
+      const { data, error } = await getTransactions(user);
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (data) {
+        const income = data
+          .filter((transaction) => transaction.type === "income")
+          .reduce((acc, transaction) => acc + transaction.amount, 0);
+        const expense = data
+          .filter((transaction) => transaction.type === "expense")
+          .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+        setTotalIncome(income);
+        setTotalExpense(expense);
+        setTotalTransactions(data.length);
+      }
     };
-    fetchTotalTransactions();
-  }, [user]);
+    setupAccountInfo();
+  }, []);
 
   return (
     <section className="col-span-4 bg-neutral-50 dark:bg-neutral-900">
       <Subtitle title="Account summary" />
       <div className="grid grid-cols-2 gap-4">
-        <SummaryCard title="Income" value="$2,000" />
-        <SummaryCard title="Expense" value="$1,000" />
-        <SummaryCard title="Balance" value="$1,000" />
+        <SummaryCard title="Income" value={totalIncome.toString()} />
+        <SummaryCard title="Expense" value={totalExpense.toString()} />
+        <SummaryCard
+          title="Balance"
+          value={(totalIncome - totalExpense).toString()}
+        />
         <SummaryCard
           title="Total Transactions"
           value={totalTransactions.toString()}

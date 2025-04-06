4f31571ef2d6f69b3141954bed96433032cbb37b
Andrés Banda <andrezh_gln7@live.com>
2025-04-06 15:11:15 -0500

    modify text field placeholder style

--- src/components/Forms/InputWithLabel.tsx
+++ src/components/Forms/InputWithLabel.tsx
@@ -1,4 +1,5 @@
 import { FaEye } from "react-icons/fa6";
+import { useState } from "react";
 
 const InputWithLabel = ({
   type = "text",
@@ -6,24 +7,36 @@ const InputWithLabel = ({
   name,
   className,
   handleChange,
-}: inputLabelProps) => (
-  <label
-    htmlFor={name}
-    className="relative flex flex-col text-gray-700 dark:text-neutral-200 font-semibold text-lg"
-  >
-    {label}
-    <input
-      className={`bg-transparent border border-gray-400 p-2 rounded-md mt-1  ${className}`}
-      type={type}
-      id={name}
-      name={name}
-      onChange={handleChange}
-    />
-    {name === "password" && (
-      <>
+}: inputLabelProps) => {
+  const [focused, setFocused] = useState(false);
+
+  return (
+    <div className="relative w-full mb-2">
+      <input
+        className={`peer bg-transparent border border-gray-300 py-3 px-4  rounded-md w-full text-white focus:outline-none ${className}`}
+        type={type}
+        id={name}
+        name={name}
+        placeholder=" "
+        onFocus={() => setFocused(true)}
+        onBlur={(e) => setFocused(e.target.value !== "")}
+        onChange={handleChange}
+      />
+      <label
+        htmlFor={name}
+        className={`
+          bg-neutral-900 px-2 rounded absolute left-2 -top-2 text-sm text-gray-400 dark:text-neutral-300 transition-all
+          peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-neutral-300
+          peer-focus:-top-2.5 peer-focus:text-sm 
+        `}
+      >
+        {label}
+      </label>
+
+      {name === "password" && (
         <button
           type="button"
-          className="bg-transparent p-0 absolute right-4 bottom-4 text-gray-500"
+          className="bg-transparent p-0 absolute right-4 top-3 text-gray-500"
           onClick={() => {
             const input = document.getElementById(name) as HTMLInputElement;
             input.type = input.type === "password" ? "text" : "password";
@@ -31,9 +44,9 @@ const InputWithLabel = ({
         >
           <FaEye />
         </button>
-      </>
-    )}
-  </label>
-);
+      )}
+    </div>
+  );
+};
 
 export default InputWithLabel;
